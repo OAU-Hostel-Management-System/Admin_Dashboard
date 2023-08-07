53207d6cab76cd82d360fa48429f473565ab49a3
Ayomide <@vayomide2@gmail.com>
2023-08-07 11:48:36 +0100

    comment

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     // kindly implement the login page using tailwind or css if preferred
-    //you can let me know if you're having 
+    //you can let me know if you're hav
     <div>Login</div>
   )
 }
