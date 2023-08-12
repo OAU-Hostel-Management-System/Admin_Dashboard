dd4b57ee51b1c0688be42101c7888276cb1938e8
sideeq12 <abdwaheed2018@gmail.com>
2023-08-12 15:27:37 +0100

    nav check

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -9,12 +9,12 @@ function Navbar() {
         <span className='text-sm'>Hostel Management Portal</span>
       </div>
       <ul className=''>
-        <li className='py-6 hover:border-r-4 hover:font-light  text-left pl-10 hover:text-[#113885] hover:border-r-[#113885] cursor-pointer hover:bg-[#CFDEFD]'>
-          <a href="/" className='flex gap-3'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+        <li className='py-6 hover:border-r-4  hover:font-light  text-left pl-10 hover:text-[#113885] hover:border-r-[#113885] cursor-pointer hover:bg-[#CFDEFD]'>
+          <a href="/"  className='flex gap-3 active:bg-red-600'><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
   <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
 </svg> Home</a>
         </li>
-        <li className='py-6 hover:border-r-4 hover:font-light text-left pl-10 hover:text-[#113885] hover:border-r-[#113885] cursor-pointer hover:bg-[#CFDEFD]'>
+        <li className='py-6 hover:border-r-4  hover:font-light text-left pl-10 hover:text-[#113885] hover:border-r-[#113885] cursor-pointer hover:bg-[#CFDEFD]'>
           <a href="/custom" className='flex gap-3'>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-building-add" viewBox="0 0 16 16">
   <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Z"/>
