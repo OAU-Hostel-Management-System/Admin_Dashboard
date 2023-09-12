0df5435db791deb69c3206167ac7ab90220c1a4b
sideeq12 <abdwaheed2018@gmail.com>
2023-09-12 20:35:05 +0100

    dashboard

--- client/src/pages/admin/hostels.jsx
+++ client/src/pages/admin/hostels.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Hostels() {
   return (
-    <div>Hostels</div>
+    <div className='m-2'>
+      <div className='bg-white p-2 flex  space-between'>
+        <div >
+          <input type="text" name="matric" id="" placeholder='search by matric' />
+        </div>
+       <div>
+        user info
+       </div>
+      </div>
+    </div>
   )
 }
 
