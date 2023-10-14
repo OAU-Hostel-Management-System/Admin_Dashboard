32c9295211bec9392ac8fd6b3f66828cd3166279
Umoh Samuel Godwin <youngking074@gmail.com>
2023-10-14 14:41:13 +0100

    search in student record

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -64,7 +64,7 @@ function Topbar() {
           OAU Student's Hostel Portal
         </p>
 
-        {currentPath === "/student_records" && (
+{/*       
           // <div className="relative">
           //   <div
           //     className="absolute inset-y-0 left-0 flex
@@ -113,66 +113,10 @@ function Topbar() {
           //     placeholder="Search students by matric..."
           //     required
           //   />
-          // </div>
+          // </div> */}
 
-          <form>
-            <label
-              for="default-search"
-              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >
-              Search
-            </label>
-            <div className="relative">
-              <div
-                className="absolute inset-y-0 left-0 flex
-         items-center pl-3 pointer-events-none"
-              >
-                <svg
-                  className="w-4 h-4 text-gray-500
-             dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
-              <input
-                onChange={(evt) => {
-                  setList(listX);
-                  if (evt.target.value == "") {
-                    setList(listX);
-                  } else {
-                    const updatedList = studentList.filter((list) =>
-                      list.matricNo.includes(evt.target.value)
-                    );
-
-                    setList(updatedList);
-                  }
-                }}
-                type="search"
-                id="search"
-                name="search"
-                className="block w-full p-4 pl-10 text-sm text-gray-900 border
-         border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500
-          focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600
-           dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-            dark:focus:border-blue-500"
-                //  ref={inputRef}
-
-                placeholder="Search students by matric..."
-                required
-              />
-            </div>
-          </form>
-        )}
+          
+    
       </div>
     </div>
   );
